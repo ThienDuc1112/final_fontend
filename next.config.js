9b5506da6fd16f70f3ec417e20ba69634a5a85ca
ThienDuc1112 <ndthien1112@gmail.com>
2024-04-25 18:56:42 +0700

    add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+import path from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -12,7 +15,14 @@ const nextConfig = {
     },
     experimental: {
       esmExternals: false,
-    }
+    },
+    webpack: (config) => {
+      config.resolve.alias = {
+       ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+       };
+      return config;
+      },
   };
   
   module.exports = nextConfig;
